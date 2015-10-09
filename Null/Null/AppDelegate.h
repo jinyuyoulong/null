dd695d12bf6f4a3db408a61269174c24f1ae2223
jinyuyoulong <fan.jinlong@qq.com>
2015-10-09 13:54:35 +0800

    create product
    添加项目

---
+++ Null/Null/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Null
+//
+//  Created by fans on 15/10/9.
+//  Copyright (c) 2015年 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Null/Null/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Null
+//
+//  Created by fans on 15/10/9.
+//  Copyright (c) 2015年 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
